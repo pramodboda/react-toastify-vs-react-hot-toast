debb8e61cfc51b9e6f5659e8ee670c78b5e77b49
pramodboda <pramodkumarboda@gmail.com>
2025-10-24 20:28:31 +0530

    Setup

--- src/components/ReactHotToast.tsx
+++ src/components/ReactHotToast.tsx
@@ -1,3 +1,4 @@
- import Typography from "@mui/material/Typography"
- const ReactHotToast = () => (<Typography variant="h6">React Hot Toast</Typography>)
+ import Typography from "@mui/material/Typography";
+ import Link from "@mui/material/Link";
+ const ReactHotToast = () => (<Link href="https://react-hot-toast.com/" target="_black"><Typography variant="h6">React Hot Toast</Typography></Link>)
  export default ReactHotToast;
--- src/components/ReactToastify.tsx
+++ src/components/ReactToastify.tsx
@@ -2,10 +2,12 @@ import { ToastContainer, toast } from 'react-toastify';
 
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
 
- const ReactToastify = () =>{
+
+const ReactToastify = () =>{
     const notify = () => toast('Wow so easy !');
-    const customSuccessNotify = () => toast('🦄 Wow so easy!', {
+    const customSuccessNotify1 = () => toast('🦄 Wow so easy!', {
         position: "top-center",
         type:"success",
         autoClose: 5000,
@@ -17,11 +19,24 @@ import Button from "@mui/material/Button";
         theme: "light",
         // transition: Bounce,
         });
+        const customSuccessNotify2 = () => toast('🦄 Wow so easy!', {
+            position: "top-center",
+            type:"success",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+            // transition: Bounce,
+            });
     return (<>
-        <Typography variant="h6">React Toastify</Typography>
+        <Link href="https://fkhadra.github.io/react-toastify/introduction" target="_black"><Typography variant="h6">React Toastify</Typography></Link>
         <div className="grid place-items-center h-dvh bg-zinc-900/15">
              <Button variant='contained' color="primary" onClick={notify}>Default Notify !</Button>
-             <Button variant='contained' color="primary" onClick={customSuccessNotify}>Custom success Notify !</Button>
+             <Button variant='contained' color="primary" onClick={customSuccessNotify1}>Custom success Notify !</Button>
+             <Button variant='contained' color="primary" onClick={customSuccessNotify2}>Custom success Notify !</Button>
              <ToastContainer />
            </div>
         </>)
