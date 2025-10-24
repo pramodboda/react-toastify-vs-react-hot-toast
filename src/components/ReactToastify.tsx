f4178bff9df749b7b5ea963c56d08fb718e96ae8
pramodboda <pramodkumarboda@gmail.com>
2025-10-24 19:58:30 +0530

    setup

--- src/components/ReactToastify.tsx
+++ src/components/ReactToastify.tsx
@@ -5,8 +5,9 @@ import Button from "@mui/material/Button";
 
  const ReactToastify = () =>{
     const notify = () => toast('Wow so easy !');
-    const customNotify = () => toast('🦄 Wow so easy!', {
+    const customSuccessNotify = () => toast('🦄 Wow so easy!', {
         position: "top-center",
+        type:"success",
         autoClose: 5000,
         hideProgressBar: false,
         closeOnClick: false,
@@ -20,7 +21,7 @@ import Button from "@mui/material/Button";
         <Typography variant="h6">React Toastify</Typography>
         <div className="grid place-items-center h-dvh bg-zinc-900/15">
              <Button variant='contained' color="primary" onClick={notify}>Default Notify !</Button>
-             <Button variant='contained' color="primary" onClick={customNotify}>Custom Notify !</Button>
+             <Button variant='contained' color="primary" onClick={customSuccessNotify}>Custom success Notify !</Button>
              <ToastContainer />
            </div>
         </>)
